05fa3be20ae86f0e9373155022c72d639da84774
vinodkumarselvaraj <vinodkumar.selvaraj@fulcrum.net>
2021-06-16 19:53:16 +0530

    dashboard blinker commit

--- app/src/main/java/com/example/flickerin/Dashboard/ViewModels/SampleViewModel1.java
+++ app/src/main/java/com/example/flickerin/Dashboard/ViewModels/SampleViewModel1.java
@@ -1,6 +1,5 @@
 package com.example.flickerin.Dashboard.ViewModels;
 
-import android.app.Activity;
 import android.content.Context;
 import android.widget.Toast;
 import androidx.lifecycle.MutableLiveData;
